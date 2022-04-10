b85119823623586aecbc51e965ed89a0950f7779
leetomlee123 <leetomlee123@gmail.com>
2022-04-10 11:20:22 +0800

    Initial commit

---
+++ test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+func main() {
+	ch2 := make(chan interface{})
+	for i := 0; i < 100000000000; i++ {
+		go httpPostForm("1234qwer@gmail.com", ch2)
+	}
+	i := 0
+	for {
+		select {
+		case u := <-ch2:
+			print(u)
+			i = 0
+		default:
+			print("ok")
+			if i > 10 {
+				goto Loop
+			}
+			i++
+			time.Sleep(time.Second)
+		}
+	}
+Loop:
+	print("执行完成")
+}
+
+func httpPostForm(email string, chanel chan interface{}) {
+	chanel <- email
+	resp, err := http.PostForm("https://fq.rs/api/v1/passport/comm/sendEmailVerify",
+		url.Values{"key": {"value"}, "email": {email}})
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+
+	chanel <- string(body)
+
+}
