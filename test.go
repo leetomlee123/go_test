40788c1c5544b78f877700f45e17c287581f1414
leetomlee123 <leetomlee123@gmail.com>
2022-04-12 18:23:23 +0800

    1111

--- test.go
+++ test.go
@@ -11,15 +11,9 @@ import (
 	"time"
 )
 
-func randomEmail() {
-	for i := 0; i < 5; i++ {
-		fmt.Println()
-	}
-}
-
 func main() {
 	ch2 := make(chan interface{})
-	for i := 0; i < 1000000; i++ {
+	for i := 0; i < 1; i++ {
 
 		go httpPostForm(string(rand.Int())+"@gmail.com", ch2)
 	}
@@ -27,10 +21,10 @@ func main() {
 	for {
 		select {
 		case u := <-ch2:
-			print(u)
+			println(u)
 			i = 0
 		default:
-			print("ok")
+			println("ok")
 			if i > 10 {
 				goto Loop
 			}
@@ -62,10 +56,22 @@ func httpPostForm(email string, chanel chan interface{}) {
 		// handle error
 	}
 	textQuoted := string(body)
+	println(textQuoted)
 	textUnquoted := textQuoted[1 : len(textQuoted)-1]
-	v, _ := zhToUnicode([]byte(textUnquoted))
-	print(v)
+	sUnicodev := strings.Split(textUnquoted, "\\u")
+	var context string
+	for _, v := range sUnicodev {
+		if len(v) < 1 {
+			continue
+		}
+		temp, err := strconv.ParseInt(v, 16, 32)
+		if err != nil {
+			panic(err)
+		}
+		context += fmt.Sprintf("%c", temp)
+	}
+	fmt.Println(context)
 
-	chanel <- textQuoted
+	chanel <- context
 
 }
