d50d97fb1db9b5ac55d4264df4c3dc136d9af7af
Alex <leetomlee123@gmail.com>
2023-05-22 19:23:01 +0800

    Update test.go

--- test.go
+++ test.go
@@ -35,6 +35,7 @@ func Loop() {
 		print(qq+"@"+q[rand.Intn(7)])
 		go SenderEmail(qq+"@"+q[rand.Intn(3)], ch2,"https://72vpn.xyz/api/v1/passport/comm/sendEmailVerify")
 		go SenderEmail(qq+"@"+q[rand.Intn(3)], ch2,"https://www.speedaccelerate.com/api/v1/passport/comm/sendEmailVerify")
+		go SenderEmail(qq+"@"+q[rand.Intn(3)], ch2,"https://shuidiy.com/api/v1/passport/comm/sendEmailVerify")
 		go Login(qq+"@"+q[rand.Intn(3)], ch2)
 		go Register(qq+"@"+q[rand.Intn(3)],  ch2)
 		//go Check(ch2)
